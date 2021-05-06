13d5100a17f642bc62b0d9ae2d39b9c3a3b3856f
revindaamalias <revindaamalia@gmail.com>
2021-05-06 14:35:37 +0800

    update product

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -1,18 +1,39 @@
 import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-  Row,
-  Col,
-} from "reactstrap";
+import firebase from "firebase";
 
 class Product extends Component {
+  // pengambilan data dari firebase
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listProduk: [],
+    };
+  }
+  ambilDataDariServerAPI = () => {
+    let ref = firebase.database().ref("/");
+    ref.on("value", (snapshot) => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  };
+
+  simpanDataKeServerAPI = () => {
+    firebase.database().ref("/").set(this.state);
+  };
+
+  componentDidMount() {
+    // komponen untuk mengecek ketika compnent telah di-mount-ing, maka panggil API
+    this.ambilDataDariServerAPI(); // ambil data dari server API lokal
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.simpanDataKeServerAPI();
+    }
+  }
+
   state = {
     listProduct: [],
   };
@@ -21,89 +42,90 @@ class Product extends Component {
     this._getProduct();
   }
 
-  _getProduct = () => {
-    fetch("http://localhost:3001/product")
-      .then((response) => response.json())
-      .then((listProduct) => {
-        this.setState({ listProduct });
-      })
-      .catch(() => {
-        alert("Gagal Ambil Product");
-      });
-  };
+  // Bagian bawah ini menggunakan fake api
 
-  _onClickBeliProduct = (product) => {
-    if (product.stock === 0) {
-      alert("Stok Kosong");
-      return;
-    }
+  // _getProduct = () => {
+  //   fetch("http://localhost:3001/product")
+  //     .then((response) => response.json())
+  //     .then((listProduct) => {
+  //       this.setState({ listProduct });
+  //     })
+  //     .catch(() => {
+  //       alert("Gagal Ambil Product");
+  //     });
+  // };
 
-    // update product stock
-    fetch(`http://localhost:3001/product/${product.id}`, {
-      method: "PATCH",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        stock: product.stock - 1,
-      }),
-    }).then(() => {
-      // saat sukses update product stock
-      // check product di keranjang
-      // saat product ada di keranjang akan masuk ke then
-      // saat product tidak ada di keranjang akan masuk ke catch
-      fetch(`http://localhost:3002/keranjang/${product.id}`)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
+  // _onClickBeliProduct = (product) => {
+  //   if (product.stock === 0) {
+  //     alert("Stok Kosong");
+  //     return;
+  //   }
 
-          throw new Error("Tidak ada di keranjang");
-        })
-        .then((lastKeranjang) => {
-          // saat product ada di keranjang
-          // update jumlah dan harga di keranjang
-          fetch(`http://localhost:3002/keranjang/${product.id}`, {
-            method: "PATCH",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              qyt: lastKeranjang.qyt + 1,
-              total: product.harga * (lastKeranjang.qyt + 1),
-            }),
-          }).then(() => {
-            // saat sukses update jumlah dan harga di keranjang
-            alert("Berhasil Menambah Keranjang");
-            this._getProduct();
-          });
-        })
-        .catch(() => {
-          const productKeranjang = {
-            id: product.id,
-            nama: product.nama,
-            harga: product.harga,
-            qyt: 1,
-            total: product.harga * 1,
-          };
+  //   // update product stock
+  //   fetch(`http://localhost:3001/product/${product.id}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       stock: product.stock - 1,
+  //     }),
+  //   }).then(() => {
+  //     // saat sukses update product stock
+  //     // check product di keranjang
+  //     // saat product ada di keranjang akan masuk ke then
+  //     // saat product tidak ada di keranjang akan masuk ke catch
+  //     fetch(`http://localhost:3002/keranjang/${product.id}`)
+  //       .then((response) => {
+  //         if (response.ok) {
+  //           return response.json();
+  //         }
 
-          // saat sukses update jumlah dan harga di keranjang
-          fetch("http://localhost:3002/keranjang", {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(productKeranjang),
-          }).then(() => {
-            alert("Berhasil Menambah Keranjang");
-            this._getProduct();
-          });
-        });
-    });
-  };
+  //         throw new Error("Tidak ada di keranjang");
+  //       })
+  //       .then((lastKeranjang) => {
+  //         // saat product ada di keranjang
+  //         // update jumlah dan harga di keranjang
+  //         fetch(`http://localhost:3002/keranjang/${product.id}`, {
+  //           method: "PATCH",
+  //           headers: {
+  //             Accept: "application/json",
+  //             "Content-Type": "application/json",
+  //           },
+  //           body: JSON.stringify({
+  //             qyt: lastKeranjang.qyt + 1,
+  //             total: product.harga * (lastKeranjang.qyt + 1),
+  //           }),
+  //         }).then(() => {
+  //           // saat sukses update jumlah dan harga di keranjang
+  //           alert("Berhasil Menambah Keranjang");
+  //           this._getProduct();
+  //         });
+  //       })
+  //       .catch(() => {
+  //         const productKeranjang = {
+  //           id: product.id,
+  //           nama: product.nama,
+  //           harga: product.harga,
+  //           qyt: 1,
+  //           total: product.harga * 1,
+  //         };
+
+  //         // saat sukses update jumlah dan harga di keranjang
+  //         fetch("http://localhost:3002/keranjang", {
+  //           method: "POST",
+  //           headers: {
+  //             Accept: "application/json",
+  //             "Content-Type": "application/json",
+  //           },
+  //           body: JSON.stringify(productKeranjang),
+  //         }).then(() => {
+  //           alert("Berhasil Menambah Keranjang");
+  //           this._getProduct();
+  //         });
+  //       });
+  //   });
 
   render() {
     return (
